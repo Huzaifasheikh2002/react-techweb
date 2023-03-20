39f995b7d15f7629f2177de05afcdc360ad94ece
huzaifasheikh2002 <huzaifanadeem621@gmail.com>
2023-03-20 20:06:47 +0500

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Tech Web</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+            Youtube
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+          </a>
+            Instagram
+          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+          </a>
+            Github
+          <a href="https://github.com/meabhisingh" target={"blank"}>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
